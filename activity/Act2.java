6b170b027631ffeebc21d88e2dfa26700b63180e
JijiMaruu <66178726+PxMorph@users.noreply.github.com>
2024-08-08 09:44:19 +0800

    Update activity

---
+++ activity/Act2.java
@@ -0,0 +1,8 @@
+public class Act2 {
+  double a, b, naverage, nmax, nmin;
+  static void findAverage(){
+   naverage = (a+b)/2;
+   System.out.println("Average : " + naverage);
+  }
+  static void 
+}
