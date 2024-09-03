3fa34e71628ab6b75d6c0a0a77ef8c4d30b7ff38
pxmorph <r-six@hotmail.com>
2024-09-03 09:29:15 +0800

    completed lab as 1

---
+++ lab/lab2.2/Task4.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Task4 {
+  public static void main(String[] args) {
+    
+    Scanner uInput = new Scanner(System.in);
+
+    int choices;
+    double val1, val2;
+
+    System.out.println("1. find area and perimeter of a rectangle");
+    System.out.println("2. find area and circumference of a circle");
+    System.out.print("Choices: ");
+    
+    choices = uInput.nextInt();
+    
+    Scanner uInput2 = new Scanner(System.in);
+    switch (choices) {
+      case 1:
+        System.out.print("length: ");
+        val1 = uInput2.nextDouble();
+        System.out.print("width: ");
+        val2 = uInput2.nextDouble();
+
+        System.out.print("area = ");
+        System.out.printf("%.2f",(val1 * val2));
+        System.out.print(" square cm , perimeter = ");
+        System.out.printf("%.2f",(val1 + val2) * 2);
+        System.out.println(" cm");
+
+        break;
+      
+      case 2:
+        System.out.print("radius: ");
+        val1 = uInput2.nextDouble();
+
+        System.out.print("area = ");
+        System.out.printf("%.2f",( Math.PI * Math.pow(val1, 2)));
+        System.out.print(" square cm, circumference = ");
+        System.out.printf("%.2f", (2*Math.PI*val1));
+        System.out.println(" cm");
+        break;
+
+      default:
+        System.out.println("Error: Wrong menu choice");
+        break;
+    }
+    uInput.close();
+    uInput2.close();
+
+  }
+
+}
