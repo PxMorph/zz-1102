d83eb42ccc93d7ed2c6e5770c6f0fa96ab92f9d0
jiji <r-six@hotmail.com>
2024-11-06 11:55:47 +0800

    coming

--- as/final/JJPuzzle.java
+++ as/final/JJPuzzle.java
@@ -1,13 +1,13 @@
 public class JJPuzzle {
   public static void main(String[] args) {
-    String [] puzzle;
-  
+    int [] puzzle = new int [20];
+     
     start(puzzle);
-    print(puzzle);
+    printJJ(puzzle);
+    OnlyGreen(puzzle);
+    OnlyYR(puzzle);
   }
-
-  public static void start(String [] puzzle) {
-    puzzle = new String[20];
+  public static void start(int [] puzzle) {
     for (int i = 0; i < 20; i++) {
       puzzle[i] = randomColor();
     }
@@ -16,29 +16,54 @@ public class JJPuzzle {
 
   //Testing methods
 
-  public static void printJJ(String [] puzzle) {
+  public static void printJJ(int [] puzzle) {
     for (int i = 0; i < 20; i++) {
       System.out.print(puzzle[i] + " | ");
     }
+    System.out.println();
+    System.out.println("0 = White | 1 = Yellow | 2 = Red | 3 = Green");
   }
 
-  //Other methods
-
-  public static String randomColor() {
-    int random = ((int)(Math.random()) * 3) + 1;
-    switch (random) {
-      case 1:
-        return "yellow";
-        break;
-
-      case 2:
-        return "red";
-        break;
-
-      case 3:
-        return "green";
-      default:
-        break;
+  public static void dupeArray(int [] puzzle, int [] newPuzzle) {
+    for (int i = 0; i < puzzle.length; i++) {
+      newPuzzle[i] = puzzle[i];
     }
   }
+
+  public static void OnlyGreen(int [] puzzle) {
+    int [] ogPuzzle = new int [20];
+    dupeArray(puzzle, ogPuzzle);
+    int greencount = 20;
+    for (int i = 0; i < ogPuzzle.length; i++) {
+      if (ogPuzzle[i] != 3) {
+        ogPuzzle[i] = 0;
+        greencount = greencount - 1;
+      }
+    }
+    System.out.println();
+    System.out.println("OnlyGreen");
+    printJJ(ogPuzzle);
+    System.out.println("Total: " + greencount);
+  }
+
+  public static void OnlyYR(int [] puzzle) {
+    int [] ogPuzzle2 = new int [20] ;
+    dupeArray(puzzle, ogPuzzle2);
+    int YRcount = 20;
+    for (int i = 0; i < ogPuzzle2.length; i++) {
+      if (ogPuzzle2[i] == 3) {
+        ogPuzzle2[i] = 0;
+        YRcount = YRcount -1;
+      }
+    }
+    System.out.println();
+    System.out.println("OnlyYR");
+    printJJ(ogPuzzle2);
+    System.out.println("Total: "+ YRcount);
+  }
+
+  public static int randomColor() {
+    return (int)(Math.random() * 3.0 + 1.0);
+  }
+
 }
